ad91cbd44ec4dd8e1339da4b3883e08631b69663
moeota <moe0925c5@gmail.com>
2022-11-09 20:07:12 +0900

    11092007

---
+++ 1test.c
@@ -0,0 +1,33 @@
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	long	   num;
+	int	   i;
+	int		minus;
+	unsigned char *str2 = (unsigned char *)str;
+
+	num = 0;
+	i = 0;
+	minus = 1;
+	while (((str2[i] >= 9) && (str2[i] <= 13)) || (str2[i] == 32))
+		i++;
+	if (str2[i] == 43)
+		i++;
+	if (str2[i] == 45)
+	{
+		minus = minus * -1;
+		i++;
+	}
+
+	while ((str2[i] >= 48) && (str2[i] <= 57))
+	{
+		num = num + str[i] - 48;
+		num = num * 10;
+		i++;
+	}
+	num = num / 10;
+	// if (num > 2147483647)
+	// 	return ((int)(num));
+	return (num * minus);
+}
